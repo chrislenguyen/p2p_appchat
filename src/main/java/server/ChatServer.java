fa03c2de8b12a986a0382c63d743ce2312298bbb
chrislenguyen <lnakhuong@gmail.com>
2020-05-08 15:40:56 +0700

    add getter, setter, hash

--- src/main/java/server/ChatServer.java
+++ src/main/java/server/ChatServer.java
@@ -1,14 +1,22 @@
 package server;
 
+import org.apache.commons.lang3.StringUtils;
+import utils.ClientInfoServer;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class ChatServer {
-    private ArrayList<ClientHandler> clientHandlerList = new ArrayList<ClientHandler>();
+    private ArrayList<ClientHandler> clientHandlerList = new ArrayList<>();
+    private HashMap<String, ClientInfoServer> clientList = new HashMap<>();
     private int serverPort;
     private ServerSocket serverSocket;
     private int numThreads = 10;
@@ -24,8 +32,13 @@ public class ChatServer {
         this.numThreads     = num;
     }
 
-    public void start() {
+    public void initServer() throws IOException {
+
+    }
+
+    public void start() throws IOException {
         System.out.println("[SERVER] Start server.");
+        initServer();
         Executor executor = Executors.newFixedThreadPool(this.numThreads);
         try {
             serverSocket = new ServerSocket(this.serverPort);
